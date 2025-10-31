3ec0e9f9d188fd58a951411b9bac8944eb712741
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-10-31 23:18:37 +0900

    create the real USPCarvana app
    - set up a new SwiftUI app for car price prediction using a CoreML model
    - includes project configuration, assets, entitlements, car names JSON, the Carvana.mlmodel, and main ContentView with form for user input and price prediction

---
+++ USPCarvana/USPCarvana/USPCarvanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  USPCarvanaApp.swift
+//  USPCarvana
+//
+//  Created by joe on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct USPCarvanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
